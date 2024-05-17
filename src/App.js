abaa6869ea134462378afdbbf5d2e464cf43cfd7
manasaniprashanth <manasaniprashanth@gmail.com>
2024-05-16 22:45:25 -0700

    react app
    react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="coming-soon">Coming Soon</div>
+    </div>
+  );
+}
+
+export default App;
